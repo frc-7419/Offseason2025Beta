96aa4d332cb25074e618c8f3062d6bf26eb3880a
shlokacharya <acharya.shlok@gmail.com>
2024-11-15 15:50:33 -0800

    ShooterCommands
    Created two separate shooter commands for same velocity and different velocity. Only implemented coast and break; will work on implementing the speeds later

---
+++ src/main/java/frc/robot/commands/SameVelShooterCommand.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class SameVelShooterCommand extends Command {
+    private final ShooterSubsystem shooterSubsystem;
+    public SameVelShooterCommand(ShooterSubsystem shooterSubsystem) {
+        this.shooterSubsystem = shooterSubsystem;
+    }
+
+    @Override
+    public void initialize() {
+        shooterSubsystem.coast();
+    }
+
+    @Override
+    public void execute() {}
+
+    @Override
+    public void end(boolean interrupted) {
+        shooterSubsystem.brake();
+    }
+
+    @Override
+    public boolean isFinished() {
+    return false;
+  }
+
+
+
+}
+
+
